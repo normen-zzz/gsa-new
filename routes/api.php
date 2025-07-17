386020a1925412309882cf25b1467a328518bad1
normanardian <normanardian24@gmail.com>
2025-07-17 17:26:59 +0700

    Fix HTTP method and case for customer update routes
    Changed the /updateDetailCustomer route from POST to PUT and corrected the case of 'Route' for /updateCustomer to ensure consistency and proper HTTP method usage.

--- routes/api.php
+++ routes/api.php
@@ -30,8 +30,8 @@ Route::get('/getCustomerById/{id}', [CustomerController::class, 'getCustomerById
 Route::post('/createCustomer', [CustomerController::class, 'createCustomer'])->middleware('auth:api');
 Route::put('/deactivateCustomerById', [CustomerController::class, 'deactiveCustomer'])->middleware('auth:api');
 Route::put('/activateCustomerById', [CustomerController::class, 'activateCustomer'])->middleware('auth:api');
-Route::post('/updateDetailCustomer', [CustomerController::class, 'updateDetailCustomer'])->middleware('auth:api');
-route::put('/updateCustomer', [CustomerController::class, 'updateCustomer'])->middleware('auth:api');
+Route::put('/updateDetailCustomer', [CustomerController::class, 'updateDetailCustomer'])->middleware('auth:api');
+Route::put('/updateCustomer', [CustomerController::class, 'updateCustomer'])->middleware('auth:api');
 
 // Master Country
 Route::post('/createCountry', [CountryController::class, 'createCountry'])->middleware('auth:api');
